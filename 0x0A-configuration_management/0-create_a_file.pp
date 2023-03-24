bd47276c8f260799ee758a69c433def12ef2ae96
FORMAT1 <olayiwolafm@gmail.com>
2023-03-24 13:28:29 -0700

    resolved o

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
  147 Bytes
 # Create a file in /tmp
 file {'/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# Install the puppet-lint package
+# Install the puppet-lint
 package { 'puppet-lint':
   ensure   => '2.5.0',
   provider => 'gem',
