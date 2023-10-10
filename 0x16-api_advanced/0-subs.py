37078f7f1d302ded4811b128c051574d3979d620
FORMAT1 <olayiwolafm@gmail.com>
2023-10-10 22:21:54 +0000

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-'''
-    function  for the number_of_subscribers
-'''
+"""fuunction  for the number_of_subscribers"""
 import requests
 from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    '''
-ii        returns the number of  subscribers for a given subreddit
-    '''
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    """ returns the number of  subscribers for a given subreddit
+    """
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return (response.json().get("data").get("subscribers"))
+    return (0)
 	
