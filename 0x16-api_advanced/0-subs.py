91c45980c71ecfa4a398c864ed97eb1e67935ee1
FORMAT1 <olayiwolafm@gmail.com>
2023-10-12 15:14:21 -0700

    restructure subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""fuunction  for the number_of_subscribers"""
+'''
+    this module contains the function number_of_subscribers
+'''
 import requests
 from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of  subscribers for a given subreddit"""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
-    """Set a custom User-Agent to avoid being blocked"""
-    headers = {'User-Agent': 'xica369'}
-
-    """Send an HTTP GET request to the Reddit API"""
-    response = requests.get(url, headers=headers)
-    
-   """ Check if the request was successful"""
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            """Extract the number of subscribers from the response"""
-            subscribers = data['data']['subscribers']
-            return subscribers
-        except (KeyError, ValueError):
-            """Invalid JSON response"""
-            return 0
-    else:
-        """Invalid subreddit or other error"""
+    '''
+        returns the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
